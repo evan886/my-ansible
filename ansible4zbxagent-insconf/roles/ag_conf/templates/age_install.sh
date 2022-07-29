3036a3b367191720758ee6e13e5d268800356ba8
Administrator <admin@example.com>
2022-07-29 17:05:00 +0800

    2022年 07月 29日 星期五 17:05:00 CST

--- ansible4zbxagent-insconf/roles/ag_conf/templates/age_install.sh
+++ ansible4zbxagent-insconf/roles/ag_conf/templates/age_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo /usr/bin/rpm -ivh https://mirrors.aliyun.com/zabbix/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
-sudo sleep 5
+sudo sleep 3
 sudo /usr/bin/yum install zabbix-agent -y
-sudo sleep 5
+sudo sleep 3
 #bak cong
 sudo sed  -i 's/127.0.0.1/172.16.0.42/g'  /etc/zabbix/zabbix_agentd.conf
 sudo cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.confbakevan
